5fd4d260e9c9fe65a7ce591e06a26f589e876ca0
PixBlezz <148706507+PixBlezz@users.noreply.github.com>
2023-12-18 08:56:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,81 @@
-// app.js
+
+
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
 
-// Connect to MongoDB (replace 'your_database_url' with your actual MongoDB connection string)
-mongoose.connect('mongodb://localhost:27017/your_database_name', { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.json());
+
+// MongoDB setup
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// Define patient schema
+// MongoDB Schema and Models
 const patientSchema = new mongoose.Schema({
-  name: String,
-  contact: String,
+  surname: String,
+  otherNames: String,
   gender: String,
-  address: String,
+  phoneNumber: String,
+  residentialAddress: String,
   emergencyName: String,
-  relationshipStatus: String,
-  encounters: [
-    {
-      patientId: mongoose.Types.ObjectId,
-      date: String,
-      time: String,
+  emergencyContact: String,
+  relationshipWithPatient: String,
+  encounters: [{
+    date: String,
+    time: String,
+    encounterType: String,
+    vitals: {
       bloodPressure: String,
+      temperature: String,
+      pulse: String,
+      spo2: String
     }
-  ]
+  }]
 });
 
 const Patient = mongoose.model('Patient', patientSchema);
 
-app.use(bodyParser.json());
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Front Desk API is running!');
-});
-
-app.post('/register', async (req, res) => {
+// Function to register patients
+app.post('/patients/register', async (req, res) => {
   const {
-    name,
-    contact,
+    surname,
+    otherNames,
     gender,
-    address,
+    phoneNumber,
+    residentialAddress,
     emergencyName,
-    relationshipStatus
+    emergencyContact,
+    relationshipWithPatient
   } = req.body;
 
-  if (!name || !contact || !gender || !address || !emergencyName || !relationshipStatus) {
-    return res.status(400).json({ error: 'All fields are required' });
-  }
-
-  const patient = new Patient({
-    name,
-    contact,
-    gender,
-    address,
-    emergencyName,
-    relationshipStatus
-  });
-
   try {
+    const patient = new Patient({
+      surname,
+      otherNames,
+      gender,
+      phoneNumber,
+      residentialAddress,
+      emergencyName,
+      emergencyContact,
+      relationshipWithPatient
+    });
+
     await patient.save();
+
     res.status(201).json(patient);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.post('/encounter/:patientId', async (req, res) => {
-  const { date, time, bloodPressure } = req.body;
-  const patientId = req.params.patientId;
+// Function to start an encounter for a patient
+app.post('/encounters/start', async (req, res) => {
+  const { patientId, date, time, encounterType } = req.body;
 
   try {
     const patient = await Patient.findById(patientId);
@@ -81,16 +84,54 @@ app.post('/encounter/:patientId', async (req, res) => {
       return res.status(404).json({ error: 'Patient not found' });
     }
 
-    patient.encounters.push({ date, time, bloodPressure });
+    const encounter = {
+      date,
+      time,
+      encounterType
+    };
+
+    patient.encounters.push(encounter);
     await patient.save();
 
-    res.status(201).json(patient.encounters[patient.encounters.length - 1]);
+    res.status(201).json(encounter);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.get('/patients', async (req, res) => {
+// Function to submit patient vitals by a nurse
+app.post('/nurse/vitals', async (req, res) => {
+  const { patientId, bloodPressure, temperature, pulse, spo2 } = req.body;
+
+  try {
+    const patient = await Patient.findById(patientId);
+
+    if (!patient) {
+      return res.status(404).json({ error: 'Patient not found' });
+    }
+
+    const currentEncounter = patient.encounters[patient.encounters.length - 1];
+    if (!currentEncounter) {
+      return res.status(400).json({ error: 'No active encounter for the patient' });
+    }
+
+    currentEncounter.vitals = {
+      bloodPressure,
+      temperature,
+      pulse,
+      spo2
+    };
+
+    await patient.save();
+
+    res.status(201).json(currentEncounter.vitals);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Function to view the list of patients by a doctor
+app.get('/doctor/patients', async (req, res) => {
   try {
     const patients = await Patient.find();
     res.json(patients);
@@ -99,7 +140,8 @@ app.get('/patients', async (req, res) => {
   }
 });
 
-app.get('/patient/:patientId', async (req, res) => {
+// Function to view detailed information about a specific patient by a doctor
+app.get('/doctor/patients/:patientId', async (req, res) => {
   const patientId = req.params.patientId;
 
   try {
